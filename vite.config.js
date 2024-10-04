6767427858047bd84c37f34db33a5a12079b6e4b
Thu N. <anhthu.ntr@gmail.com>
2024-10-04 00:59:54 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue({
-    template: {
-      compilerOptions: {
-        isCustomElement: (tag) => tag.startsWith('font-awesome-icon'),
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith("font-awesome-icon"),
+        },
       },
-    },
-  })],
+    }),
+  ],
+  base: "/my-portfolio/",
 });
